96eee246104f08baff66c4c126c8e2f32ebbd599
Ofuya Tamara <Tammyo>
2026-05-28 23:05:34 +0100

    Remove CloudFront references from outputs

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -8,16 +8,6 @@ output "s3_bucket_arn" {
   value       = aws_s3_bucket.frontend.arn
 }
 
-output "cloudfront_distribution_id" {
-  description = "CloudFront distribution ID"
-  value       = aws_cloudfront_distribution.frontend.id
-}
-
-output "cloudfront_domain_name" {
-  description = "CloudFront domain name"
-  value       = aws_cloudfront_distribution.frontend.domain_name
-}
-
 output "redis_endpoint" {
   description = "Redis endpoint"
   value       = aws_elasticache_cluster.redis.cache_nodes[0].address
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,19 +3,9 @@ output "alb_dns_name" {
   value       = module.compute.alb_dns_name
 }
 
-output "cloudfront_domain_name" {
-  description = "CloudFront domain - use this as your frontend URL"
-  value       = module.storage.cloudfront_domain_name
-}
-
 output "s3_bucket_name" {
   description = "S3 bucket name for frontend deployment"
   value       = module.storage.s3_bucket_name
-}
-
-output "cloudfront_distribution_id" {
-  description = "CloudFront distribution ID for cache invalidation"
-  value       = module.storage.cloudfront_distribution_id
 }
 
 output "redis_endpoint" {
